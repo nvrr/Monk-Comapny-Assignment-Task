c9331ee1708fac2a9f1c2d766f80bc148eca8245
nvrr <sureshvarma809@gmail.com>
2025-03-02 17:01:57 +0530

    animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 
 
+import { useEffect, useState } from 'react';
 import EligibilityRules from './components/EligibilityRules';
+import './App.css'
 
 function App() {
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setShowContent(true), 1000); // Wait for the letter to open
+  }, []);
  
   return (
-    <>
-    <div className='bg-[#F1F2F4] min-w-screen min-h-screen py-10 px-52 '>
+    <div className='wrapper'>
+    <div className={`letter-container ${showContent ? "show" : ""} bg-[#F1F2F4] min-w-screen min-h-screen py-10 px-52`} >
       
-       <div className="p-4 text-sm mx-auto bg-white shadow-lg rounded-2xl   ">
+       <div className="letter-content p-4 text-sm mx-auto bg-white shadow-lg rounded-2xl   ">
        <h6 className='text-base font-semibold'>Rule</h6>
       <div className='font-medium'>The offer will be triggered based on the rules in this section</div>
 <div className='h-[1px] w-full bg-[#EBEBEB] my-3'></div>
@@ -18,7 +25,7 @@ function App() {
 
       </div>
       </div>
-    </>
+    </div>
   )
 }
 
